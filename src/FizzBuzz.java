9ca7da50d2313da006334e66ba278e240c16549f
Royangliu <royangliu@gmail.com>
2024-09-09 16:15:36 -0400

    Update completing up to task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,6 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        // for (int i = 1; i < 100; i++) {
         int i = 1;
         while (i < 100) {
 
@@ -13,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        String[] strings = {"1","2","3"};
+        FizzBuzz.main(strings);
+    }
+}
